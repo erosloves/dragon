db7e6e197db4a9a93fec99d61ffb19b21b9161d4
3ricwebdev <ericxdd@yandex.ru>
2024-07-10 16:16:50 +0800

    ui update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,22 +6,7 @@ import Image from "next/image";
 const Header = () => {
   return (
     <div className={styles.header}>
-      <Link href={"/"}>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100%",
-            fontSize: "30px",
-            fontFamily: "NeueHaasDisplayThin",
-            fontWeight: "lighter",
-            letterSpacing: "8px",
-          }}
-        >
-          The Dragon Model Management
-        </div>
-      </Link>
+      <Link href={"/"}>The Dragon Model Management</Link>
     </div>
   );
 };
